3610a1c9255b6f54f251c6389c77ed660ba031ea
Nassim <achnassim34@gmail.com>
2026-05-07 10:28:09 +0200

    travail initial

---
+++ app.py
@@ -0,0 +1,35 @@
+import random
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+from typing import TypedDict
+
+taux_contamination: float = 0.1
+taux_guerison: float= 0.1
+init_population: float = 1000
+
+S = [init_population-1]
+I = [1]
+R = [0]
+
+def simulation(day_nb: int,tc: float,tg: float,popu: int, s=list,i=list,r=list):
+
+    for d in range(0,day_nb):
+
+        nouveauxInfectes = tc * float(i[d]) * (s[d] / popu)
+        nouveauxRetablis = tg * float(i[d])
+
+        s.append(s[d] - nouveauxInfectes)
+        i.append(i[d] + nouveauxInfectes - nouveauxRetablis)
+        r.append(r[d] + nouveauxRetablis)
+
+    sir = {
+        "susceptibles": s,
+        "infectes": i,
+        "r": r,
+    }
+
+    return sir;
+
+sir = simulation(30,taux_contamination,taux_guerison, init_population, S,I,R);
+print(sir)
